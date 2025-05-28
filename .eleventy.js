db700d7c521f22becb522478a1f44b81f2147ed0
Christopher II Lajom <christopheriilajom@gmail.com>
2025-05-28 23:23:30 +0800

    Initialized.

---
+++ .eleventy.js
@@ -0,0 +1,20 @@
+import { DateTime } from 'luxon';
+
+export default async function(eleventyConfig) {
+	// Configure Eleventy
+    eleventyConfig.addPassthroughCopy('./src/styles.css');
+    eleventyConfig.addPassthroughCopy('./src/assets');
+
+    eleventyConfig.addFilter('postDate', (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+    })
+
+    eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`);
+
+    return {
+        dir: {
+            input: 'src',
+            output: 'public',
+        }
+    }
+};
